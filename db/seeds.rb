fbc465e8f3cb1f6d4e8fef6034c668bab9b44688
TomomitsuKeruma <78133385+TomomitsuKeruma@users.noreply.github.com>
2021-03-17 14:38:51 +0900

    Revert "テストデータ追記"
    This reverts commit 3c37a057836ccf4e54a04b0ce5a8126915b933d0.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,1 @@
-Product.delete_all
-#...
-Product.create(title: 'Programming Ruby 1.9',
-  description:
-  % { <p>
-     Ruby is is the fastest growing and most exciting dynamic language 
-     out there. If you need toget working programs delivered fast,
-     you should add Ruby to your toolbox. 
-     </p> },
-  image_url: 'ruby.jpg',
-  price: 49.95)
-#...
+Product.delete_all  #  ...  Product.create(title: 'Programming Ruby 1.9',  description:  %{<p>  Ruby is the fastest growing and most exciting dynamic language  out there. If you need to get working programs delivered fast,  you should add Ruby to your toolbox.  </p>},  image_url: 'ruby.jpg',  price: 49.95)  #  ... 
